20e584c0e437d66f12fbfecd11f686394d759731
glvnv <polin-mart@ya.ru>
2019-03-20 15:14:41 +0300

    Added button, textview and event handler.

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn;
+    TextView mainTextView;
+    Button mainButton;
+
+    int clicks=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.button);
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        mainTextView = findViewById(R.id.main_textview);
+        mainTextView.setText("Кнопка нажата "+clicks+" раз(а).");
+    }
+
+    @Override
+    public void onClick(View v) {
+        clicks = clicks+1;
+        mainTextView.setText("Кнопка нажата "+clicks+" раз(а).");
     }
 }
